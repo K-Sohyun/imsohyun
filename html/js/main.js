11f25ee10cefcca1ad8556d277300af1872f4add
K-Sohyun <ksoh912@gmail.com>
2024-05-24 19:31:00 +0900

    포트폴리오 영역 완성

--- html/js/main.js
+++ html/js/main.js
@@ -1,6 +1,17 @@
 $(function () {
 
-    //헤더 스크롤
+    //로딩화면
+    $('#loader').delay(1000).fadeOut(1000);
+
+    //스크롤
+    $('a').click(function () {
+        $('html, body').animate({ 
+            scrollTop: $($.attr(this, 'href')).offset().top
+         }, 500);
+        return false;
+    });
+
+    //헤더변경
     var lastT = 0;
     $(window).scroll(function () {
         var nowT = $(this).scrollTop();
@@ -17,10 +28,10 @@ $(function () {
         lastT = nowT;
     });
 
-    //포트폴리오 로드
+    //포트폴리오
     $('.task_list > li').hide();
     $('.task_list > li').slice(0, 6).css("display", "block");
-    $("#load_btn").click(function (e) {
+    $('#load_btn').click(function (e) {
         e.preventDefault();
         $('.task_list > li:hidden').slice(0, 6).fadeIn(300).css('display', 'block');
         if ($('.task_list > li:hidden').length == 0) {
@@ -28,22 +39,7 @@ $(function () {
         }
     });
 
-
-    //GSAP
-    gsap.registerPlugin(ScrollTrigger);
-
-    ScrollTrigger.create({
-        trigger: "#about .img_box",
-        start: "top center",
-        end: "bottom center",
-        toggleClass: "on"
-    });
-
-    ScrollTrigger.create({
-        trigger: "#about .text_box",
-        start: "top center",
-        end: "bottom center",
-        toggleClass: "on"
-    });
+    //AOS
+    AOS.init();
 
 });
